33458c24b45c2a59de1eb2e192cab15748830a8a
mrdang <khacquyet.dang@gmail.com>
2017-10-06 20:59:08 +0200

    bind key arrow to move map

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,13 +6,49 @@ import HallCell from '../data/HallCell';
 import RoomCell from '../data/RoomCell';
 
 class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+            x: 0,
+            y: 0
+        }
+    }
+    onKeyPress = (event) => {
+        console.log("onKeyPress ", event.key);
+        switch (event.key) {
+            case "ArrowDow":
+                var x = this.state.x + 1;
+                this.setState({ x: x });
+                break;
+            case "ArrowUp":
+                var x = this.state.x - 1;
+                this.setState({ x: x });
+                break;
+            case "ArrowLeft":
+                var y = this.state.y - 1;
+                this.setState({ y: y });
+                break;
+            case "ArrowRight":
+                var y = this.state.y + 1;
+                this.setState({ y: y });
+                break;
+        }
+        if (event.key == 'Enter') {
+            this.setState({ value: event.target.value })
+        }
+    }
     render() {
 
         var game2DArr = this.props.ground;
         if (game2DArr === null) {
             return <div></div>;
         }
-       
+
+        game2DArr = _.slice(game2DArr, this.state.x, this.state.x + 30);
+        game2DArr = game2DArr.map(row => {
+            return _.slice(row, this.state.y, this.state.y + 30);
+        });
+
         var gameMapDiv = game2DArr.map((row, indexRow) => {
             var divRow = row.map((cell, indexCell) => {
                 if (cell instanceof WallCell) {
@@ -40,8 +76,9 @@ class Game extends Component {
             return <div key={indexRow} className="GameRow">{divRow}</div>
         });
         return (
-            <div>Hello world
-                <div className="gameMap" >
+            <div tabIndex="0" onKeyDown={this.onKeyPress}>Hello world
+                <div
+                    className="gameMap" >
                     {gameMapDiv}
                 </div>
             </div>
